513731ba57893993ce22f1716416c3f9cf1fb211
Sunil BK <67497228+sunil9813@users.noreply.github.com>
2022-09-30 18:07:41 +0545

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,10 +6,10 @@ import { nav } from "../../assets/data/data"
 import { Link } from "react-router-dom"
 
 export const Header = () => {
-  /* window.addEventListener("scroll", function () {
+   window.addEventListener("scroll", function () {
     const header = this.document.querySelector(".header")
     header.classList.toggle("active", this.window.scrollY > 100)
-  })*/
+  }) 
   return (
     <>
       <header className='header'>
